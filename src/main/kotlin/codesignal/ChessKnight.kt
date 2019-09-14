e7bacd6f604c3585fe3cbda9f04e76808c08b3f7
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2019-09-14 22:41:57 +0200

    solutions for the Chess Knight challenge

---
+++ src/main/kotlin/codesignal/ChessKnight.kt
@@ -0,0 +1,19 @@
+package codesignal
+
+import kotlin.math.abs
+
+class ChessKnight {
+
+    val chessKnight = { c: String ->
+        var s = 0
+        val x = c[0].toInt() - 96
+        val y = c[1].toInt() - 48
+        val l = listOf(-2,-1,1,2)
+        l.forEach { v ->
+            l.forEach { w ->
+                if (x + v in 1..8 && y + w in 1..8 && abs(v) != abs(w)) s++
+            }
+        }
+        s
+    }
+}
