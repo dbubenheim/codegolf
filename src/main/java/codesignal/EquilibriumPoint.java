9e34d0015bd03d01ecbfd9dfa1eced36bcfa3bdd
Daniel Bubenheim <daniel.bubenheim@gmail.com>
2020-08-04 22:24:00 +0200

    adds java solutions to some challenges. and updates the gitignore file

--- src/main/java/codesignal/EquilibriumPoint.java
+++ src/main/java/codesignal/EquilibriumPoint.java
@@ -1,4 +1,20 @@
 package codesignal;
 
+import java.util.List;
+
 public class EquilibriumPoint {
+
+    public int equilibriumPoint(List<Integer> a) {
+        var c = 0;
+        var b = a.stream().mapToInt(x -> x).sum();
+        for (var x : a) b += x;
+        var i = 0;
+        for (var x : a) {
+            b -= x;
+            if (c == b) return ++i;
+            c += x;
+            i++;
+        }
+        return -1;
+    }
 }
--- src/main/java/hackerrank/EndOfFile.java
+++ src/main/java/hackerrank/EndOfFile.java
@@ -1,4 +1,15 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 public class EndOfFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        while(scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.println(++i + ". " + line);
+        }
+    }
 }
